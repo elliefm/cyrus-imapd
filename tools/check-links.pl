4e23330d7fc9e788f245665a550c906d43e0c69b
ellie timoney <ellie@fastmail.com>
2016-09-08 17:05:58 +1000

    wip

--- tools/check-links.pl
+++ tools/check-links.pl
@@ -44,9 +44,15 @@ use strict;
 use warnings;
 
 use Data::Dumper;
+use File::Basename;
 use File::Find;
 use Getopt::Std;
 use HTML::TokeParser::Simple;
+
+my %options;
+my %counts;
+my %files;
+my $htmlroot;
 
 sub usage
 {
@@ -63,6 +69,7 @@ sub find_files
 
     my $wanted = sub {
         my $ext = (split /\./, $_)[-1];
+        return if not $ext;
         return if not grep { $_ eq $ext } @{$exts};
         push @files, $File::Find::name;
     };
@@ -72,11 +79,101 @@ sub find_files
     return @files;
 }
 
-my %options;
-my $htmlroot;
+sub out_warn
+{
+    my ($message, $count_str) = @_;
 
-usage if not getopts("", \%options);
+    $counts{'warning'} ++;
+    $counts{$count_str} ++ if $count_str;
+    print STDERR 'warning: ', $message, "\n";
+}
 
+sub out_error
+{
+    my ($message, $count_str) = @_;
+
+    $counts{'error'} ++;
+    $counts{$count_str} ++ if $count_str;
+    print STDERR 'error: ', $message, "\n";
+}
+
+sub out_verbose
+{
+    return if not $options{v} and not $options{d};
+
+    my ($message) = @_;
+    print $message, "\n";
+}
+
+sub out_debug
+{
+    return if not $options{d};
+
+    my ($message) = @_;
+    print $message, "\n";
+}
+
+sub parse_file
+{
+    my ($filename, $data) = @_;
+
+    my $p = HTML::TokeParser::Simple->new(file => $filename);
+
+    while (my $tag = $p->get_tag) {
+        next if not $tag->is_start_tag();
+
+        my $id = $tag->get_attr('id');
+        if ($id) {
+            out_error "$filename: duplicate id: $id" if exists $data->{anchors}->{$id};
+            $data->{anchors}->{$id} = {};
+        }
+
+        my $href = $tag->get_attr('href');
+        push @{$data->{hrefs}}, $href if $href;
+    }
+}
+
+sub check_hrefs
+{
+    my ($files) = @_;
+
+    foreach my $filename (sort keys %{$files}) {
+        out_verbose "checking $filename hrefs...";
+        my (undef, $path, undef) = fileparse($filename);
+        out_debug "$filename is in $path";
+
+        foreach my $href (@{$files->{$filename}->{hrefs}}) {
+            my ($link, $anchor, $junk) = split /#/, $href;
+            out_error "bad link: $href" if $junk;
+
+            if ($link) {
+                # skip mailto links
+                next if $link =~ m{^mailto:};
+
+                # XXX todo - check external links
+                next if $link =~ m{^\w+://};
+
+                if ($link =~ m{^/}) {
+                    $link = "$htmlroot/$link";
+                }
+                else {
+                    $link = "$path/$link";
+                }
+
+                out_error "$filename: link to nonexistent file: $href ($link)" if not -f $link;
+            }
+
+            # XXX fix the link path so we can find it in our hash
+            # XXX then mark it as seen
+            # XXX and if we're linking to an anchor, make sure that anchor exists
+            # XXX and mark it as soon too
+        }
+    }
+}
+
+#############################################################################
+
+usage if not getopts("dv", \%options);
 $htmlroot = shift @ARGV // q{.};
 
 print Dumper \%options;
@@ -84,7 +181,19 @@ print Dumper \%options;
 print "htmlroot: $htmlroot\n";
 print "ok\n";
 
-my @files = find_files $htmlroot;
+%files = map { ($_ => {}) } find_files $htmlroot;
 
-print Dumper \@files;
+#print Dumper \%files;
 
+# parse the files
+while (my ($file, $data) = each %files) {
+    out_verbose "parsing $file...";
+    parse_file($file, $data);
+}
+
+print Dumper \%files;
+
+# process our discoveries
+check_hrefs(\%files);
+
+print Dumper \%counts;
